34ef94402465a41696a7698cdd5bac9d951cc36c
lodwr <l.odwrot@gmail.com>
2017-05-06 11:20:37 +0200

    Lunched project

--- src/main/java/com/pwr/controller/PatientController.java
+++ src/main/java/com/pwr/controller/PatientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pwr.model.PatientTO;
@@ -18,16 +19,19 @@ public class PatientController {
 	private IPatientService patientService;
 
 	@RequestMapping(path = "all", method = RequestMethod.GET)
+  @ResponseBody
 	public List<PatientTO> findAllPatients() {
 		return patientService.findAllPatients();
 	}
-	
+
 	@RequestMapping(path = "create", method = RequestMethod.GET)
+  @ResponseBody
 	public void createPatient() {
 		patientService.createPatient();
 	}
 
 	@RequestMapping(path = "hello")
+  @ResponseBody
 	public String hello() {
 		return "Hello, world";
 	}
