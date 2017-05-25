39c445e9b92591c8dafb41c707d7468a7d68f7da
lodwr <l.odwrot@gmail.com>
2017-05-25 23:18:14 +0200

    Clearing repo

--- app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app',
-  template: '<app-nav></app-nav><router-outlet></router-outlet>',
-} as Component)
-export class AppComponent {
-}
