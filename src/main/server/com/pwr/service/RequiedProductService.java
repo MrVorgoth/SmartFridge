a69993348cd56a0c02511f539b3790c0122de6ed
cenuran <202697@student.pwr.edu.pl>
2017-06-05 22:46:59 +0200

    Update RequiedProductService.java

--- src/main/server/com/pwr/service/RequiedProductService.java
+++ src/main/server/com/pwr/service/RequiedProductService.java
@@ -1,15 +1,37 @@
 package com.pwr.service;
 
+import com.pwr.mappers.RequiedProductMapper;
+import com.pwr.model.RequiedProductEntity;
+import com.pwr.model.RequiedProductTO;
 import com.pwr.repository.RequiedProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by Magda Zielonka on 19.05.2017.
+ * Created by Ania on 04.06.2017.
  */
 @Service
 public class RequiedProductService implements IRequiedProductService {
 
     @Autowired
     private RequiedProductRepository requiedProductRepository;
+
+    @Override
+    public List<RequiedProductTO> findAllRequiedProd() {
+        List<RequiedProductTO> result = new ArrayList<RequiedProductTO>();
+        requiedProductRepository.findAll().forEach(requiedProduct -> result.add(RequiedProductMapper.mapRequiedProduct(requiedProduct)));
+        return result;
+    }
+
+    @Override
+    public void createRequiedProd(int quantity) {
+        RequiedProductEntity requiedProductEntity = new RequiedProductEntity();
+        requiedProductEntity.setQuantity(quantity);
+
+        requiedProductRepository.save(requiedProductEntity);
+
+    }
 }
