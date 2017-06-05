d6050003e87d9a6a2acb28fa92466d351ae9307e
cenuran <202697@student.pwr.edu.pl>
2017-06-05 23:18:47 +0200

    Update IShoppingListService.java

--- src/main/server/com/pwr/service/IShoppingListService.java
+++ src/main/server/com/pwr/service/IShoppingListService.java
@@ -1,7 +1,16 @@
 package com.pwr.service;
 
+import com.pwr.model.UserShoppingListTO;
+
+import java.util.List;
+
 /**
- * Created by Magda Zielonka on 19.05.2017.
+ * Created by Ania on 04.06.2017.
  */
 public interface IShoppingListService {
+
+    List<UserShoppingListTO> findAllShoppingList();
+    List<UserShoppingListTO> findShoppingListByID(long id);
+    void createShoppingList(int quantity);
+    void delateShoppingList(long id);
 }
