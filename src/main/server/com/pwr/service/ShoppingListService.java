0d98533e3a78a0204c454bd0cf63a5247718662a
cenuran <202697@student.pwr.edu.pl>
2017-06-05 22:47:35 +0200

    Update ShoppingListService.java

--- src/main/server/com/pwr/service/ShoppingListService.java
+++ src/main/server/com/pwr/service/ShoppingListService.java
@@ -1,15 +1,51 @@
 package com.pwr.service;
 
+import com.pwr.mappers.ShoppingListMapper;
+import com.pwr.model.UserShoppingListEntity;
+import com.pwr.model.UserShoppingListTO;
 import com.pwr.repository.ShoppingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by Magda Zielonka on 19.05.2017.
+ * Created by Ania on 04.06.2017.
  */
 @Service
 public class ShoppingListService implements IShoppingListService {
 
     @Autowired
     private ShoppingListRepository shoppingListRepository;
+
+    @Override
+    public List<UserShoppingListTO> findAllShoppingList() {
+        List<UserShoppingListTO> result = new ArrayList<UserShoppingListTO>();
+        shoppingListRepository.findAll().forEach(shoppingList -> result.add(ShoppingListMapper.mapUser(shoppingList)));
+        return result;
+    }
+
+    @Override
+    public List<UserShoppingListTO> findShoppingListByID(long id) {
+        List<UserShoppingListTO> result = new ArrayList<UserShoppingListTO>();
+        for (UserShoppingListEntity userShoppingListEntity : shoppingListRepository.findAllById(id)) {
+            result.add(ShoppingListMapper.mapUser(userShoppingListEntity));
+        }
+        return result;
+    }
+
+    @Override
+    public void createShoppingList(int quantity) {
+        UserShoppingListEntity userShoppingListEntity = new UserShoppingListEntity();
+        userShoppingListEntity.setQuantity(quantity);
+
+        shoppingListRepository.save(userShoppingListEntity);
+
+    }
+
+    @Override
+    public void delateShoppingList(long id) {
+        shoppingListRepository.delete(id);
+    }
 }
