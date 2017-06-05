138a22e0489b9098e2435f64f2c74c2c190d8b33
cenuran <202697@student.pwr.edu.pl>
2017-06-05 23:17:47 +0200

    Update ShoppingListRepository.java

--- src/main/server/com/pwr/repository/ShoppingListRepository.java
+++ src/main/server/com/pwr/repository/ShoppingListRepository.java
@@ -1,7 +1,14 @@
 package com.pwr.repository;
 
 import com.pwr.model.ProductBaseEntity;
+import com.pwr.model.UserShoppingListEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShoppingListRepository extends JpaRepository<ProductBaseEntity, Long> {
+import java.util.List;
+/**
+ * Created by Ania on 04.06.2017.
+ */
+public interface ShoppingListRepository extends JpaRepository<UserShoppingListEntity, Long> {
+
+    List<UserShoppingListEntity> findAllById(long id);
 }
