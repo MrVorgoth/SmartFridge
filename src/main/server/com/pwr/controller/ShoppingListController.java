e9b05e3131cf754655e3be2e3d8b8b48abea400e
cenuran <202697@student.pwr.edu.pl>
2017-06-05 22:40:10 +0200

    Update ShoppingListController.java

--- src/main/server/com/pwr/controller/ShoppingListController.java
+++ src/main/server/com/pwr/controller/ShoppingListController.java
@@ -1,13 +1,15 @@
 package com.pwr.controller;
 
+import com.pwr.model.UserShoppingListTO;
 import com.pwr.repository.ShoppingListRepository;
 import com.pwr.service.IShoppingListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
- * Created by Magda Zielonka on 18.05.2017.
+ * Created by Ania on 04.06.2017.
  */
 @RestController
 @RequestMapping(path = "shoppingList")
@@ -17,4 +19,29 @@ public class ShoppingListController {
     private IShoppingListService shoppingListService;
     @Autowired
     private ShoppingListRepository shoppingListRepository;
+
+    @RequestMapping(path = "create", method = RequestMethod.GET)
+    @ResponseBody
+    public String createShoppingList(@RequestParam("quantity") int quantity){
+        shoppingListService.createShoppingList(quantity);
+
+        return "OK";
+    }
+
+    @RequestMapping(path = "all", method = RequestMethod.GET)
+    public List<UserShoppingListTO> findAllShoppingList(){
+        return shoppingListService.findAllShoppingList();
+    }
+
+    @RequestMapping(path = "delete", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void deleteShoppingList(@RequestParam("id") long id) {
+
+        shoppingListService.delateShoppingList(id);
+    }
+
+    @RequestMapping(path = "search", method = RequestMethod.GET)
+    public List<UserShoppingListTO> findShoppingListByID(@RequestParam("id") long id) {
+        return shoppingListService.findShoppingListByID(id);
+    }
 }
