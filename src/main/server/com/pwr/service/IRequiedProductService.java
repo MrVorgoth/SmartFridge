258e491ffaa19bcc3eda8dab9fd212712c4c7f02
cenuran <202697@student.pwr.edu.pl>
2017-06-05 22:45:33 +0200

    Update IRequiedProductService.java

--- src/main/server/com/pwr/service/IRequiedProductService.java
+++ src/main/server/com/pwr/service/IRequiedProductService.java
@@ -1,7 +1,14 @@
 package com.pwr.service;
 
+import com.pwr.model.RequiedProductTO;
+
+import java.util.List;
+
 /**
- * Created by Magda Zielonka on 19.05.2017.
+ * Created by Ania on 04.06.2017.
  */
 public interface IRequiedProductService {
+
+    List<RequiedProductTO> findAllRequiedProd();
+    void createRequiedProd(int quantity);
 }
